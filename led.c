1616bd2f04b60bf32834e61ecbc425f94e81a37b
STONE Tech <68054217+STONE-Technology@users.noreply.github.com>
2020-07-10 16:14:15 +0800

    HARDWARE

--- led.c
+++ led.c
@@ -9,7 +9,7 @@ void LED_Init(void)
  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB|RCC_APB2Periph_GPIOE, ENABLE);	 //ENABLE CLOCK
 	
  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2;				 //-->PB config
- GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		 // PP OUT
+ GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		 // PP OUT 
  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		 //GPIO SPEED 50MHz
  GPIO_Init(GPIOB, &GPIO_InitStructure);					 
  GPIO_SetBits(GPIOB,GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2);						 //config out hight
